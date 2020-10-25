e7ee8de282066908559d959bf25ae5653a88991f
wawyed <wawyed@gmail.com>
2020-10-25 12:32:50 +0000

    test(filterPipe): add unit tests to filter pipe

--- src/app/pipes/filter.pipe.spec.ts
+++ src/app/pipes/filter.pipe.spec.ts
@@ -1,8 +1,41 @@
 import { FilterPipe } from './filter.pipe';
 
 describe('FilterPipe', () => {
+  let pipe: FilterPipe<string>;
+
+  function filterValue(value): Array<string> {
+    return pipe.transform(value, 'a', (criteria: string, val: string) => val.includes(criteria));
+  }
+
+  beforeEach(() => {
+    pipe = new FilterPipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new FilterPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  describe('when called transform with a nil value', () => {
+    let result: Array<string>;
+
+    beforeEach(() => {
+      result = filterValue(null);
+    });
+
+    it('should return null', () => {
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('when called transform with a value', () => {
+    let result: Array<string>;
+
+    beforeEach(() => {
+      result = filterValue(['abc', 'bc']);
+    });
+
+    it('should filter the array based on the function provided', () => {
+      expect(result).toEqual(['abc']);
+    });
   });
 });
